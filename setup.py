b6296bd97dc21a43a24e07cf35d049fd0bf3ba3b
[Faozia Fariha] <[faoziafariha058@gmail.com]>
2026-02-14 02:49:38 +0600

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Faozia Fariha",
+    author_email="faoziafariha058@gmail.com",
+    packages=find_packages()
+) 
